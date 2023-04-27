b1a4b199ae0eea524b2987c0a01475d3f19a8efc
Kazuaki Miyamoto <kazuaki.miyamoto.job@gmail.com>
2023-04-27 15:23:25 +0900

    feat: #13 トップページの雛形を作成する

--- config/routes.rb
+++ config/routes.rb
@@ -1,8 +1,7 @@
 Rails.application.routes.draw do
   # Define your application routes per the DSL in https://guides.rubyonrails.org/routing.html
 
-  # Defines the root path route ("/")
-  # root "articles#index"
-
+  root 'home#index'
+  resources :home, only: :index
   devise_for :users
 end
