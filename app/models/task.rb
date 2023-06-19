c775f865b170ca793f11ea8852f3ae5ea2b42d6f
Kazuaki Miyamoto <kazuaki.miyamoto.job@gmail.com>
2023-06-19 21:40:22 +0900

    feat: #25 バリデーションと日本語訳を追加する

--- app/models/task.rb
+++ app/models/task.rb
@@ -1,3 +1,19 @@
 class Task < ApplicationRecord
   belongs_to :project
+
+  validates :name, presence: true
+  validate :check_period
+  validates :is_done, inclusion: [true, false]
+
+  private
+
+  def check_period
+    if start_at.nil? || end_at.nil?
+      return
+    end
+
+    if start_at > end_at
+      errors.add(:end_at, "は開始日以降にしてください")
+    end
+  end
 end
