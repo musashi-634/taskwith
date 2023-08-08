f35e0faa7347a359404b7d0ebd3b3a824a6448b2
Kazuaki Miyamoto <kazuaki.miyamoto.job@gmail.com>
2023-08-08 19:10:46 +0900

    test: #52 system specにおいて、ユーザーの組織設定を追加する

--- spec/system/tasks_spec.rb
+++ spec/system/tasks_spec.rb
@@ -1,7 +1,7 @@
 require 'rails_helper'
 
 RSpec.describe 'Tasks', type: :system do
-  let(:user) { create(:user) }
+  let(:user) { create(:user, :with_organization) }
 
   before { login_as(user, :scope => :user) }
 
