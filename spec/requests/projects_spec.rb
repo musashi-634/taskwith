9a07de736fde01f44914409d985da911cdda5a2c
Kazuaki Miyamoto <kazuaki.miyamoto.job@gmail.com>
2023-08-08 19:08:15 +0900

    test: #52 request specに、組織に所属していない場合のテストを追加する

--- spec/requests/projects_spec.rb
+++ spec/requests/projects_spec.rb
@@ -1,36 +1,79 @@
 require 'rails_helper'
 
 RSpec.describe "Projects", type: :request do
+  let(:user) { create(:user) }
+
+  before { sign_in user }
+
   describe "GET /projects" do
-    let(:user) { create(:user) }
-    let!(:project) { create(:project, is_archived: false) }
+    context 'ユーザーが組織に所属している場合' do
+      let(:project) { create(:project, is_archived: false) }
 
-    before do
-      sign_in user
-      get projects_path
+      before do
+        project.organization.users << user
+        get projects_path
+      end
+
+      it 'プロジェクト情報を取得できること' do
+        expect(response.body).to include project.name
+        expect(response.body).to include project.description
+      end
+
+      it '表示中のプロジェクトの数量を取得できること' do
+        expect(response.body).to include "#{Project.where(is_archived: false).count}件のプロジェクトを表示中"
+      end
     end
 
-    it 'プロジェクト情報を取得できること' do
-      expect(response.body).to include project.name
-      expect(response.body).to include project.description
+    context 'ユーザーが組織に所属していない場合' do
+      before { get projects_path }
+
+      it '組織作成ページにリダイレクトされること' do
+        expect(response).to redirect_to new_organization_path
+      end
+    end
+  end
+
+  describe "GET /projects/new" do
+    context 'ユーザーが組織に所属している場合' do
+      let!(:organization) { create(:organization, users: [user]) }
+
+      before { get new_project_path }
+
+      it 'プロジェクト作成ページにアクセスできること' do
+        expect(response).to have_http_status 200
+        expect(response.body).to include '新規プロジェクト作成'
+      end
     end
 
-    it '表示中のプロジェクトの数量を取得できること' do
-      expect(response.body).to include "#{Project.where(is_archived: false).count}件のプロジェクトを表示中"
+    context 'ユーザーが組織に所属していない場合' do
+      before { get new_project_path }
+
+      it '組織作成ページにリダイレクトされること' do
+        expect(response).to redirect_to new_organization_path
+      end
     end
   end
 
   describe "GET /projects/archived" do
-    let(:user) { create(:user) }
-    let!(:archived_project) { create(:project, :archived) }
+    context 'ユーザーが組織に所属している場合' do
+      let(:archived_project) { create(:project, :archived) }
 
-    before do
-      sign_in user
-      get projects_archived_index_path
+      before do
+        archived_project.organization.users << user
+        get projects_archived_index_path
+      end
+
+      it 'アーカイブ済みのプロジェクト名を取得できること' do
+        expect(response.body).to include archived_project.name
+      end
     end
 
-    it 'アーカイブ済みのプロジェクト名を取得できること' do
-      expect(response.body).to include archived_project.name
+    context 'ユーザーが組織に所属していない場合' do
+      before { get projects_archived_index_path }
+
+      it '組織作成ページにリダイレクトされること' do
+        expect(response).to redirect_to new_organization_path
+      end
     end
   end
 end
--- spec/requests/tasks_spec.rb
+++ spec/requests/tasks_spec.rb
@@ -3,17 +3,31 @@ require 'rails_helper'
 RSpec.describe "Tasks", type: :request do
   describe "GET /projects/:project_id/tasks" do
     let(:user) { create(:user) }
-    let(:task) { create(:task) }
+    let(:project) { create(:project) }
 
-    before do
-      sign_in user
-      get project_tasks_path(task.project)
+    before { sign_in user }
+
+    context 'ユーザーが組織に所属している場合' do
+      let!(:task) { create(:task, project: project) }
+
+      before do
+        project.organization.users << user
+        get project_tasks_path(project)
+      end
+
+      it 'タスク情報を取得できること' do
+        expect(response.body).to include task.name
+        expect(response.body).to include task.start_at.to_fs(:date)
+        expect(response.body).to include task.end_at.to_fs(:date)
+      end
     end
 
-    it 'タスク情報を取得できること' do
-      expect(response.body).to include task.name
-      expect(response.body).to include task.start_at.to_fs(:date)
-      expect(response.body).to include task.end_at.to_fs(:date)
+    context 'ユーザーが組織に所属していない場合' do
+      before { get project_tasks_path(project) }
+
+      it '組織作成ページにリダイレクトされること' do
+        expect(response).to redirect_to new_organization_path
+      end
     end
   end
 end
