63f5c978f81b7a4c4405ad54d7ed90f73fed7ba3
Kazuaki Miyamoto <kazuaki.miyamoto.job@gmail.com>
2023-08-16 15:24:12 +0900

    feat: #52 他の組織のタスク一覧ページへのアクセス制限を追加する

--- app/controllers/tasks_controller.rb
+++ app/controllers/tasks_controller.rb
@@ -1,4 +1,6 @@
 class TasksController < ApplicationController
+  before_action :block_user_belongs_to_other_organization
+
   def index
     @project = Project.find(params[:project_id])
     @tasks = @project.tasks.order(:row_order)
@@ -8,6 +10,12 @@ class TasksController < ApplicationController
   end
 
   private
+
+  def block_user_belongs_to_other_organization
+    if Project.find(params[:project_id]).organization != current_user.organization
+      redirect_to projects_path
+    end
+  end
 
   def create_timeline_dates(base_date, half_time_span_year)
     start_date = base_date.years_ago(half_time_span_year).beginning_of_month
--- spec/requests/tasks_spec.rb
+++ spec/requests/tasks_spec.rb
@@ -3,26 +3,39 @@ require 'rails_helper'
 RSpec.describe "Tasks", type: :request do
   describe "GET /projects/:project_id/tasks" do
     let(:user) { create(:user) }
-    let(:project) { create(:project) }
 
     before { sign_in user }
 
     context 'ユーザーが組織に所属している場合' do
-      let!(:task) { create(:task, project: project) }
+      let!(:organization) { create(:organization, users: [user]) }
 
-      before do
-        project.organization.users << user
-        get project_tasks_path(project)
+      context '所属組織のプロジェクトの場合' do
+        let(:project) { create(:project, organization: organization) }
+        let!(:task) { create(:task, project: project) }
+
+        before { get project_tasks_path(project) }
+
+        it 'タスク情報を取得できること' do
+          expect(response.body).to include task.name
+          expect(response.body).to include task.start_at.to_fs(:date)
+          expect(response.body).to include task.end_at.to_fs(:date)
+        end
       end
 
-      it 'タスク情報を取得できること' do
-        expect(response.body).to include task.name
-        expect(response.body).to include task.start_at.to_fs(:date)
-        expect(response.body).to include task.end_at.to_fs(:date)
+      context '他の組織のプロジェクトの場合' do
+        let(:project) { create(:project) }
+
+        before { get project_tasks_path(project) }
+
+        it 'プロジェクト一覧ページにリダイレクトされること' do
+          expect(response).to redirect_to projects_path
+        end
       end
     end
 
     context 'ユーザーが組織に所属していない場合' do
+      let(:project) { create(:project) }
+
       before { get project_tasks_path(project) }
 
       it '組織作成ページにリダイレクトされること' do
