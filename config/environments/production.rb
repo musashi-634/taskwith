e2e6e283efb8c7539990ef949096f72e8af7be40
Kazuaki Miyamoto <kazuaki.miyamoto.job@gmail.com>
2023-08-31 14:36:39 +0900

    chore: #58 action_mailer.default_url_optionsを設定する

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -68,7 +68,7 @@ Rails.application.configure do
   # Set this to true and configure the email server for immediate delivery to raise delivery errors.
   config.action_mailer.raise_delivery_errors = true
 
-  config.action_mailer.default_url_options = { host: 'https://example.com' }
+  config.action_mailer.default_url_options = { host: 'https://taskwith-059601560e8c.herokuapp.com' }
   config.action_mailer.delivery_method = :smtp
   config.action_mailer.smtp_settings = {
     address: 'smtp.gmail.com',
