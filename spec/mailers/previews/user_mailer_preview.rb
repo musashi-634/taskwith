bb2695071d059d6a197925cdb8d34ff3990ca183
Kazuaki Miyamoto <kazuaki.miyamoto.job@gmail.com>
2023-09-13 15:10:10 +0900

    test: #58 招待メールのプレビューを追加する

--- spec/mailers/previews/user_mailer_preview.rb
+++ spec/mailers/previews/user_mailer_preview.rb
@@ -1,3 +1,14 @@
 # Preview all emails at http://localhost:3000/rails/mailers/user_mailer
 class UserMailerPreview < ActionMailer::Preview
+  def invitation_instructions
+    inviter = User.first
+    invitee = User.new(
+      email: 'test@example.com',
+      invited_by_type: inviter.class,
+      invited_by_id: inviter.id,
+      invitation_created_at: Time.zone.now
+    )
+    invitation_token = 'abcdef'
+    UserMailer.invitation_instructions(invitee, invitation_token)
+  end
 end
