c32e83ffb6757e37ec4c21672cf086b8efd49c9a
Kazuaki Miyamoto <kazuaki.miyamoto.job@gmail.com>
2023-09-30 09:05:03 +0900

    feat: #26 SortableJSを導入する

---
+++ app/javascript/controllers/sortable_controller.js
@@ -0,0 +1,11 @@
+import { Controller } from "@hotwired/stimulus"
+import { Sortable } from "sortablejs"
+
+export default class extends Controller {
+
+  connect() {
+    Sortable.create(this.element, {
+      animation: 150,
+    });
+  }
+}
--- config/importmap.rb
+++ config/importmap.rb
@@ -7,3 +7,4 @@ pin "@hotwired/stimulus-loading", to: "stimulus-loading.js", preload: true
 pin_all_from "app/javascript/controllers", under: "controllers"
 pin "bootstrap", to: "bootstrap.min.js", preload: true
 pin "@popperjs/core", to: "popper.js", preload: true
+pin "sortablejs", to: "https://ga.jspm.io/npm:sortablejs@1.15.0/modular/sortable.esm.js"
