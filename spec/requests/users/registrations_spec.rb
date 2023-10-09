4c2147cad81dc9712ab7baa225dadc0089fa8a7a
Kazuaki Miyamoto <kazuaki.miyamoto.job@gmail.com>
2023-10-09 16:00:09 +0900

    refactor: #16 usersのrequest specを役割ごとにファイル分割する

--- spec/requests/users/registrations_spec.rb
+++ spec/requests/users/registrations_spec.rb
@@ -1,29 +1,6 @@
 require 'rails_helper'
 
-RSpec.describe "Users", type: :request do
-  describe "GET /users/sign_in" do
-    context 'ログインしていない場合' do
-      before { get new_user_session_path }
-
-      it 'ログインページに遷移できること' do
-        expect(response).to have_http_status 200
-      end
-    end
-
-    context 'ログインしている場合' do
-      let(:user) { create(:user) }
-
-      before do
-        sign_in user
-        get new_user_session_path
-      end
-
-      it 'プロジェクト一覧ページにリダイレクトされること' do
-        expect(response).to redirect_to projects_path
-      end
-    end
-  end
-
+RSpec.describe "Users::Registrations", type: :request do
   describe "GET /users/sign_up" do
     context 'ログインしていない場合' do
       before { get new_user_registration_path }
