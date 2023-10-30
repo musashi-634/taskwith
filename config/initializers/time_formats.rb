b418c3f8792902153888187b35cff38e6222e0f1
Kazuaki Miyamoto <kazuaki.miyamoto.job@gmail.com>
2023-10-30 16:52:13 +0900

    feat: #80 タスク一覧ページのレイアウトを調整する

--- config/initializers/time_formats.rb
+++ config/initializers/time_formats.rb
@@ -1,5 +1,6 @@
 JAPANESE_DAYS_OF_WEEK = %w(日 月 火 水 木 金 土)
 
 Date::DATE_FORMATS[:month] = '%Y/%-m'
-Date::DATE_FORMATS[:date] = '%Y/%-m/%-d'
+Date::DATE_FORMATS[:date] = '%Y/%m/%d'
+Date::DATE_FORMATS[:short_date] = '\'%y/%m/%d'
 Date::DATE_FORMATS[:japanese_day_of_week] = ->(date) { JAPANESE_DAYS_OF_WEEK[date.strftime('%w').to_i] }
